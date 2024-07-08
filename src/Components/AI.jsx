c54a39da4e616703047d6a8fd58d2cc63d1343bf
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-07-08 15:16:31 +0530

    color

--- src/Components/AI.jsx
+++ src/Components/AI.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import torntry from '../assets/torntry.png'
 import './AI.css';
+import overlay from '../assets/overlay.jpg'
 
 function AI() {
   return (
     <div>
         <img src={torntry} alt='' className='torn'></img>
         <div className="backAI">
-            
+
     </div>
     </div>
   )
