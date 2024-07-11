3a362e9c23ade90fe858f45c9e129477a14a27ac
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-07-11 17:05:05 +0530

    bestest

--- src/Components/HomeComponents/AI.jsx
+++ src/Components/HomeComponents/AI.jsx
@@ -2,30 +2,32 @@ import React, { useEffect, useRef } from 'react';
 import torntry from '../../assets/torntry.png';
 import './AI.css';
 import Vortex from './Vortex.jsx';
+import { Link } from 'react-router-dom';
 
 const AI = () => {
-  const h1Ref = useRef(null);
+  const h1Ref1 = useRef(null);
+  const h1Ref2 = useRef(null);
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   useEffect(() => {
-    const handleMouseOver = (event) => {
+    const handleMouseOver = (event, ref) => {
       let iteration = 0;
       let interval = null;
 
       clearInterval(interval);
 
       interval = setInterval(() => {
-        event.target.innerText = event.target.innerText
+        ref.current.innerText = ref.current.innerText
           .split("")
           .map((letter, index) => {
             if (index < iteration) {
-              return event.target.dataset.value[index];
+              return ref.current.dataset.value[index];
             }
             return letters[Math.floor(Math.random() * 26)];
           })
           .join("");
 
-        if (iteration >= event.target.dataset.value.length) {
+        if (iteration >= ref.current.dataset.value.length) {
           clearInterval(interval);
         }
 
@@ -33,11 +35,15 @@ const AI = () => {
       }, 30);
     };
 
-    const h1Element = h1Ref.current;
-    h1Element.addEventListener('mouseover', handleMouseOver);
+    const h1Element1 = h1Ref1.current;
+    const h1Element2 = h1Ref2.current;
+
+    h1Element1.addEventListener('mouseover', (event) => handleMouseOver(event, h1Ref1));
+    h1Element2.addEventListener('mouseover', (event) => handleMouseOver(event, h1Ref2));
 
     return () => {
-      h1Element.removeEventListener('mouseover', handleMouseOver);
+      h1Element1.removeEventListener('mouseover', (event) => handleMouseOver(event, h1Ref1));
+      h1Element2.removeEventListener('mouseover', (event) => handleMouseOver(event, h1Ref2));
     };
   }, [letters]);
 
@@ -46,15 +52,16 @@ const AI = () => {
       <img src={torntry} alt="torn effect" className="torn" />
       
       <div className="hyperplexed-container">
-        <h1 ref={h1Ref} data-value="HYPERPLEXED">HYPERPLEXED</h1>
+        <h1 ref={h1Ref1} data-value="HAVE A DOUBT ?">HAVE A DOUBT?</h1>
+        <h1 ref={h1Ref2} data-value="LET A.I DO IT FOR YOU">LET A.I DO IT FOR YOU</h1>
       </div>
 
       <Vortex />
 
       <div className="containerbtn">
-        <a className="btnAI" href="#">
-          <span className="btnInner">Button</span>
-        </a>
+        <Link to="/Doubt" className="btnAI">
+          <span className="btnInner">DOUBT</span>
+        </Link>
       </div>
     </div>
   );
