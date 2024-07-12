7d28130709781e07557b37339c00cebc14f74160
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-07-12 12:55:38 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,13 +4,16 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="paper">
-      <p>Want to contribute? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit in officiis modi rem quam est inventore alias architecto rerum consequatur.</p>
-
+<mark>      
+  <span className='foot'>Want to contribute? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit in officiis modi rem quam est inventore alias architecto rerum consequatur.</span>
 <div className="social">
       <a href='#' target='blank'> <i class="fa fa-linkedin"></i></a>
       <a href='#' target='blank'> <i class="fa fa-twitter"></i></a>
       <a href='#' target='blank'> <i class="fa fa-github"></i></a>
 </div>
+<svg className='footer1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 60" preserveAspectRatio="none" stroke="#E8F109"><path d="m 3.518915,27.827324 c 55.429038,4.081 111.581115,5.822 167.117815,2.867 22.70911,-1.208 45.39827,-0.601 68.126,-0.778 28.38172,-0.223 56.76078,-1.024 85.13721,-1.33 24.17378,-0.261 48.4273,0.571 72.58114,0.571"/></svg>
+<svg className='footer1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 60" preserveAspectRatio="none" stroke="#F1FB02"><path d="m 3.518915,27.827324 c 55.429038,4.081 111.581115,5.822 167.117815,2.867 22.70911,-1.208 45.39827,-0.601 68.126,-0.778 28.38172,-0.223 56.76078,-1.024 85.13721,-1.33 24.17378,-0.261 48.4273,0.571 72.58114,0.571"/></svg>
+</mark>
     </div>
   );
 };
