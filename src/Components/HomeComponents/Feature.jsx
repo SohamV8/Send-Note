71267aeaaecb9c7816795b64f9a167bf4795a94a
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-07-14 09:37:00 +0530

    final

--- src/Components/HomeComponents/Feature.jsx
+++ src/Components/HomeComponents/Feature.jsx
@@ -6,13 +6,13 @@ import SparklesCore from './SparklesCore';
 
 function Feature() {
   return (
-    <>      
+    <>
       <img src={tornlast} alt='torn paper' className='last-torn-feat' />
       <div className="FeatureContainer">
         <div className="SparklesPreview">
           <h1 className='F-Title' data-text="WE GOT EVERYTHING COVER FOR YOU">WE GOT EVERYTHING COVER FOR YOU</h1>
           <h3 className='F-SubTitle' data-text="Everything Everywhere All at Once">Everything Everywhere All at Once</h3>
-          <div className="gradient-line"></div>     
+          <div className="gradient-line"></div>
           <div className="sparkles-container">
             <SparklesCore
               background="transparent"
