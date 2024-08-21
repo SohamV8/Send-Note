42ca67a1dc34b4eb022109a40efcf4ffff7040dc
unknown <sohamvashists8@gmail.com>
2024-08-22 00:09:57 +0530

    contribute

--- src/Pages/Contribute.jsx
+++ src/Pages/Contribute.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Form from '../Components/Form'
+import Instruction from '../Components/Instruction'
 
 function Contribute() {
   return (
     <div>
+      <Instruction />
       <Form />
     </div>
   )
