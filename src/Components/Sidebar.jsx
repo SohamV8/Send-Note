9c30e3059e6fadd69043ddd44b82c74326ba4254
unknown <sohamvashists8@gmail.com>
2024-11-11 13:58:39 +0530

    Notes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -25,6 +25,16 @@ const Sidebar = () => {
             <li><a href="#">Engineering Graphics Design</a></li>
             <li><a href="#">Physics</a></li>
             <li><a href="#">Chemistry</a></li>
+            <li><a href="#">Basic of Mechanical Engineering</a></li>
+            <li><a href="#">Basics of Electrical & Electronics Engineering</a></li>
+            <li><a href="#">Effective Communication-I</a></li>
+            <li><a href="#">Effective Communication-II</a></li>
+            <li><a href="#">FOCP-I</a></li>
+            <li><a href="#">FOCP-II</a></li>
+            <li><a href="#">FOCP-I</a></li>
+            <li><a href="#">FOCP-II</a></li>
+            <li><a href="#">Mathematics-I</a></li>
+            <li><a href="#">Mathematics-II</a></li>
           </ul>
         </li>
         <li className="sub-menu">
@@ -33,10 +43,13 @@ const Sidebar = () => {
             <i className={`arrow ${activeMenu === 2 ? 'down' : 'right'}`}></i>
           </a>
           <ul className={`submenu ${activeMenu === 2 ? 'expanded' : ''}`}>
-            <li><a href="#">EWA</a></li>
-            <li><a href="#">ADA</a></li>
-            <li><a href="#">DBMS</a></li>
-            <li><a href="#">CS</a></li>
+            <li><a href="#">Computer Networks</a></li>
+            <li><a href="#">Data Structures</a></li>
+            <li><a href="#">DE&CA</a></li>
+            <li><a href="#">Discrete Mathematics</a></li>
+            <li><a href="#">ENTERPRISE WEB APPLICATION</a></li>
+            <li><a href="#">MiddleWare Framework</a></li>
+            <li><a href="#">Entrepreneur</a></li>
           </ul>
         </li>
         <li className="sub-menu">
@@ -45,9 +58,11 @@ const Sidebar = () => {
             <i className={`arrow ${activeMenu === 3 ? 'down' : 'right'}`}></i>
           </a>
           <ul className={`submenu ${activeMenu === 3 ? 'expanded' : ''}`}>
-            <li><a href="#">Engineering Graphics Design</a></li>
-            <li><a href="#">Physics</a></li>
-            <li><a href="#">Chemistry</a></li>
+            <li><a href="#">Web Frameworks</a></li>
+            <li><a href="#">No SQL Databases</a></li>
+            <li><a href="#">Introduction to AI&ML</a></li>
+            <li><a href="#">Human Values & Professional Ethics</a></li>
+            <li><a href="#">Operating System</a></li>
           </ul>
         </li>
         <li className="sub-menu">
